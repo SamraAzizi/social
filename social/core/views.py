4681e22cafc3825355bfe85fde49f6854ab167aa
SamraAzizi <fahimaazizi369@gmail.com>
2024-10-19 22:51:19 +0500

    url-routing

--- social/core/views.py
+++ social/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse 
 
 # Create your views here.
 
 def index(request):
-    return httpResponse('<h1> Welcome to Social App</h1>')
+    return HttpResponse('<h1> Welcome to Social App</h1>')
