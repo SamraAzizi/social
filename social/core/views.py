5cebac72019cc323de72cc2a0dc5ad83b40ac697
Samra <118079600+SamraAzizi@users.noreply.github.com>
2024-11-20 20:51:24 +0500

    Update views.py

--- social/core/views.py
+++ social/core/views.py
@@ -1,13 +1,4 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
-from django.contrib import messages
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
-from .models import Profile, Post, LikePost, FollowersCount
-from itertools import chain
-import random
 
-# Create your views here.
 
 def index(request):
     user_object = User.objects.get(username=request.user.username)
